379af84be1314976a565d3dce97f2bb7cff537ba
KarlosElyas <karloselyas2@gmail.com>
2023-08-05 14:46:25 -0300

    Script da cena slot

--- inventory/inventory.gd
+++ inventory/inventory.gd
@@ -1,13 +1,11 @@
 extends PanelContainer
-
-#cena do slot
-const Slot = preload("res://inventory/slot.tscn")
+# SCRIPT do inventário, limpa e popula o invetário de acordo com a data
+#do player, assim a Interface Gráfica exibira os itens de acordo com a data
+const Slot = preload("res://inventory/slot.tscn")#cena do slot
 
 @onready var item_grid = $MarginContainer/ItemGrid
 
-func _ready():
-	var inv_data = preload("res://test_inv.tres")
-	populate_item_grid(inv_data.slot_datas)
+
 
 func populate_item_grid(slot_datas: Array[SlotData]) -> void:
 	for child in item_grid.get_children():
@@ -16,3 +14,6 @@ func populate_item_grid(slot_datas: Array[SlotData]) -> void:
 	for slot_data in slot_datas:
 		var slot = Slot.instantiate()
 		item_grid.add_child(slot) # adicionando os slots do vetor no grid da UI
+		
+		if slot_data: # cria o slot com base no vetor
+			slot.set_slot_data(slot_data)
