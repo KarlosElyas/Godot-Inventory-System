b081838e4ec5627ad8004c2756ce624e719324d0
KarlosElyas <karloselyas2@gmail.com>
2023-08-07 14:30:31 -0300

    Coletar items do chão

--- inventory/inventory_interface.gd
+++ inventory/inventory_interface.gd
@@ -1,5 +1,6 @@
 extends Control
 # SCRIPT do nó InventoryInterface em main
+signal drop_slot_data(slot_data: SlotData)
 
 var grabbed_slot_data: SlotData
 var external_inventory_owner
@@ -61,3 +62,12 @@ func update_grabbed_slot():
 		grabbed_slot.set_slot_data(grabbed_slot_data)
 	else:
 		grabbed_slot.hide()
+
+func _on_gui_input(event):
+	pass
+#	if event is InputEventMouseButton and event.is_pressed() \
+#		and grabbed_slot_data:
+#		match event.button_index:
+#			MOUSE_BUTTON_LEFT:
+#				drop_slot_data.emit(grabbed_slot_data)
+#				print("drop data")
